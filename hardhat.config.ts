90e5f04f7b5b2e64b9522b0639dc910ebe469124
lukaw3d <luka@oasisprotocol.org>
2026-01-05 19:08:36 +0100

    config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,23 +1,11 @@
 import * as dotenv from "dotenv";
 
 import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
-import "solidity-coverage";
+import "@nomicfoundation/hardhat-verify";
 
 dotenv.config();
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -46,6 +34,10 @@ const config: HardhatUserConfig = {
     ],
   },
   networks: {
+    emerald: {
+      url: "https://emerald.oasis.io",
+      chainId: 42262,
+    },
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
       accounts:
@@ -56,8 +48,11 @@ const config: HardhatUserConfig = {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
   },
+  sourcify: {
+    enabled: true,
+  },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    enabled: false,
   },
 };
 
